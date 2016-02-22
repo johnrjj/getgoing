7fb4bb3acbef0e5a68b255bb4821b508ac722a67
John J <johnrjj@gmail.com>
2016-02-22 15:04:42 -0500

    Playing around with return vals

--- src/jjpackage/hello/hello.go
+++ src/jjpackage/hello/hello.go
@@ -2,11 +2,38 @@ package main
 
 import "fmt"
 
+type Salutation struct {
+  name string
+  greeting string
+}
+
+const (
+  PI = 3.14
+  Language = "Go"
+)
+
+const (
+  A = iota
+  B
+  C
+)
+
+func Greet(salutation Salutation) {
+  _, alternateMessage := CreateMessage(salutation.name, salutation.greeting)
+  fmt.Println(alternateMessage)
+}
+
+func CreateMessage(name, greeting string) (message string, alternate string) {
+  message = greeting + " " + name
+  alternate = "HEY! "  + name
+  return
+}
+
 func main()  {
+  var s = Salutation { "John", "Hello!" }
+  Greet(s)
 
-  var message string
-  a, b, c := 1, 2, 3 // decleration and initilization, implicit
-  message = "hello go world"
-
-  fmt.Println(message, a, b, c);
+  fmt.Println(PI)
+  fmt.Println(Language)
+  fmt.Println(A, B, C)
 }
