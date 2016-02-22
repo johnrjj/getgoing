fee4cf9ffb0a4c2980db6ffe8d35c40905130175
John J <johnrjj@gmail.com>
2016-02-22 10:59:03 -0500

    Initial commit

---
+++ src/jjpackage/hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+  var message string
+  a, b, c := 1, 2, 3 // decleration and initilization, implicit
+  message = "hello go world"
+
+  fmt.Println(message, a, b, c);
+}
